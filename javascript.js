85e740c124bff68494e9bc16c6621740d03a8158
gcamaci <77690094+gcamaci@users.noreply.github.com>
2022-02-23 21:07:07 -0600

    Creating ui

--- javascript.js
+++ javascript.js
@@ -1,86 +1,111 @@
-//Global Scope
-let computerScore = 0;
-let playerScore = 0;
+let computerScore=0;
+let playerScore=0;
 
-//Generate Random Number and assign it to Rock Paper or Scissors
-function computerPlay(choice){
+//selecting userScore
+const userScore = document.querySelector('#user');
+
+//selecting enemy score 
+const garyScore = document.querySelector('#enemy');
+
+//selecting outcome 
+const outcome = document.querySelector('#outcome')
+
+let userChoice;
+//selecting buttons and adding event listeners 
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+
+    button.addEventListener('click', () => {
+        userChoice = button.id;
+        playGame();
+        
+        
+        
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function computerPlay(){
     choice = Math.floor(Math.random() * 3);
     if(choice===0){
-        return 'rock';
+        return 'grass';
     }else if(choice===1){
-        return 'paper';
+        return 'water';
     }else{
-        return 'scissors';
+        return 'fire';
     }
-}
-//create a function for logic of rock paper scissors
-/*one round, takes two parameters and compares to determine a winner. when winner is decided
-computer or player score is incremented by 1 with ++. 
-*/
+};
 
-function round(playerSelection,computerChoice){
-    if(playerSelection === computerChoice){
-        return 'Tie';
+function round(playerSelection){
+    const computerSelection = computerPlay();
+    if(playerSelection === computerSelection){
+        outcome.textContent = 'Tie';
 
     }
-    else if(playerSelection === 'rock'){
-        if(computerChoice === 'scissors'){
-            playerScore++
-            return 'You Win! Rock beats Scissors' ;
+    else if(playerSelection === 'grass'){
+        if(computerSelection === 'water'){
+            playerScore ++
+            userScore.textContent = playerScore;
+            outcome.textContent = 'Supper Effective! Grass beats Water'
             
         }else{
-            computerScore++
-            return 'You lost! Paper eats Rock';
+            computerScore++;
+            garyScore.textContent = computerScore;
+            outcome.textContent = 'You Lost! Fire beats Grass';
         }
 
     }
-    else if(playerSelection === 'paper'){
-        if(computerChoice=== 'rock'){
-            playerScore++
-            return 'you win paper beats rock';
+    else if(playerSelection === 'fire'){
+        if(computerSelection=== 'grass'){
+            playerScore++;
+            userScore.textContent = playerScore;
+            outcome.textContent = 'Super Effective! Fire beats Grass';
         }else{
-            computerScore++
-            return 'you lost scissors beat paper';
+            computerScore++;
+            garyScore.textContent =computerScore;
+            outcome.textContent = 'You Lost, Water beats Fire';
         }
 
     }
-    else if(playerSelection === 'scissors'){
-        if(computerChoice === 'paper'){
-            playerScore++
-            return 'You win scissors beats Paper'
+    else if(playerSelection === 'water'){
+        if(computerSelection === 'fire'){
+            playerScore++;
+            userScore.textContent = playerScore;
+            outcome.textContent = 'Super Effective! Water beats Fire';
         }else{
-            computerScore++
-            return "you lose Rock Bears Scissors "
+            computerScore++;
+            garyScore.textContent = computerScore;
+            outcome.textContent = "You Lost! Grass Beats water ";
         }
     }  
 }
-/*
-starts the game 
-*/
+
 function playGame(){
-    //creating playerSelection scope
-    let playerSelection;
-    // while  both counters are less than 5, prompt user for input and save as selection
-    //log and invoke round function to determine winner
-    while(computerScore < 5 && playerScore < 5){
-        playerSelection = prompt("rock paper or scissors",playerSelection);
-        console.log(round(playerSelection,computerPlay()));
-        if(computerScore === 5){
-            return `You lost to the Computer ${computerScore} to ${playerScore}`
-        }
-        else if(playerScore === 5){
-            return `You beat the Computer ${playerScore} to ${computerScore}`
+    if (playerScore <= 4 && computerScore <= 4){
+        round(userChoice);
 
-        }
-        else{
-            console.log(`Computer: ${computerScore} Your Score: ${playerScore}`)
-        }
-    }  
-}
+    }else if(playerScore === 5){
+        console.log("you win")
 
-
-
-//---------Ideas for Change--------//
-//make computer play function declaration a function Expression.
-// see if you can then use it in the playGame function instead of actually calling function. 
-
+    }else if(computerScore){
+        console.log('you lost')
+    }
+    else{
+        return;
+    }
+};
