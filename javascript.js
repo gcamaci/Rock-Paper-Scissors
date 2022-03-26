eabe4f9fe4f700b4bb32eee8ac8cb16d92222020
gcamaci <77690094+gcamaci@users.noreply.github.com>
2022-03-25 22:56:03 -0500

    Restructured, can still redo JS

--- javascript.js
+++ javascript.js
@@ -1,85 +1,156 @@
 let computerScore=0;
 let playerScore=0;
 
+
+const userImage = document.querySelector('.image1')
+const compImage = document.querySelector('.image2')
 //selecting userScore
-const userScore = document.querySelector('#user');
+const userScore = document.querySelector('.user-score');
 
 //selecting enemy score 
-const garyScore = document.querySelector('#enemy');
+const opScore = document.querySelector('.computer-score');
 
 //selecting outcome 
 const outcome = document.querySelector('#outcome')
 
 let userChoice;
 //selecting buttons and adding event listeners 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.button');
 buttons.forEach((button) => {
 
     button.addEventListener('click', () => {
         userChoice = button.id;
-        playGame();
-        
-        
-        
+        userImage.innerHTML = " ";
+        compImage.innerHTML = " "
+        playGame(); 
     });
 });
 
 function computerPlay(){
     choice = Math.floor(Math.random() * 3);
     if(choice===0){
-        return 'grass';
+        return 'rock';
     }else if(choice===1){
-        return 'water';
+        return 'paper';
     }else{
-        return 'fire';
+        return 'scissors';
     }
 };
 
 function round(playerSelection){
-    const computerSelection = computerPlay();
+    const computerSelection = computerPlay(); 
+
     if(playerSelection === computerSelection){
         outcome.textContent = 'Tie';
 
+        const img1 = document.createElement('img');
+        img1.src = `images/${playerSelection}.png`;
+        img1.classList.add('playerImage');
+        userImage.appendChild(img1);
+
+        const img2 = document.createElement('img')
+        img2.src = `images/${playerSelection}.png`;
+        img2.classList.add('compImage');
+        compImage.appendChild(img2);
+
     }
-    else if(playerSelection === 'grass'){
-        if(computerSelection === 'water'){
+    else if(playerSelection === 'rock'){
+        if(computerSelection === 'scissors'){
             playerScore ++
             userScore.textContent = playerScore;
-            outcome.textContent = 'Supper Effective! Grass beats Water'
+            outcome.textContent = 'You Win, Rock Beats Scissors!';
+            const img2 = document.createElement('img');
+            const img1 = document.createElement('img');
+            img1.src = 'images/rock.png';
+            img1.classList.add('playerImage');
+            userImage.appendChild(img1);
+
+            img2.src = 'images/scissors.png';
+            img2.classList.add('compImage');
+            compImage.appendChild(img2)
             
         }else{
             computerScore++;
-            garyScore.textContent = computerScore;
-            outcome.textContent = 'You Lost! Fire beats Grass';
+            opScore.textContent = computerScore;
+            outcome.textContent = 'You Lost, Paper beats Rock!';
+            const img2 = document.createElement('img');
+            const img1 = document.createElement('img');
+            img1.src = 'images/rock.png';
+            img1.classList.add('playerImage');
+            userImage.appendChild(img1);
+            img2.src = 'images/paper.png';
+            img2.classList.add('compImage');
+            compImage.appendChild(img2)
         }
 
     }
-    else if(playerSelection === 'fire'){
-        if(computerSelection=== 'grass'){
+    else if(playerSelection === 'paper'){
+        if(computerSelection=== 'rock'){
             playerScore++;
             userScore.textContent = playerScore;
-            outcome.textContent = 'Super Effective! Fire beats Grass';
+            outcome.textContent = 'You Win, Paper Beats Rock';
+
+            const img1 = document.createElement('img');
+            img1.src = 'images/paper.png';
+            img1.classList.add('playerImage');
+            userImage.appendChild(img1);
+
+            const img2 = document.createElement('img')
+            img2.src = 'images/rock.png';
+            img2.classList.add('compImage');
+            compImage.appendChild(img2)
         }else{
             computerScore++;
-            garyScore.textContent =computerScore;
-            outcome.textContent = 'You Lost, Water beats Fire';
+            opScore.textContent =computerScore;
+            outcome.textContent = 'You Lost, Scissors Beats Paper';
+
+            const img1 = document.createElement('img');
+            img1.src = 'images/paper.png';
+            img1.classList.add('playerImage');
+            userImage.appendChild(img1);
+
+            const img2 = document.createElement('img')
+            img2.src = 'images/scissors.png';
+            img2.classList.add('compImage');
+            compImage.appendChild(img2);
         }
 
     }
-    else if(playerSelection === 'water'){
-        if(computerSelection === 'fire'){
+    else if(playerSelection === 'scissors'){
+        if(computerSelection === 'paper'){
             playerScore++;
             userScore.textContent = playerScore;
-            outcome.textContent = 'Super Effective! Water beats Fire';
+            outcome.textContent = 'You Win, Scissors Beats Paper';
+
+            const img1 = document.createElement('img');
+            img1.src = 'images/scissors.png';
+            img1.classList.add('playerImage');
+            userImage.appendChild(img1);
+
+            const img2 = document.createElement('img')
+            img2.src = 'images/paper.png';
+            img2.classList.add('compImage');
+            compImage.appendChild(img2);
         }else{
             computerScore++;
-            garyScore.textContent = computerScore;
-            outcome.textContent = "You Lost! Grass Beats water ";
+            opScore.textContent = computerScore;
+            outcome.textContent = "You Lost, Rock Beats Scissors";
+
+            const img1 = document.createElement('img');
+            img1.src = 'images/scissors.png';
+            img1.classList.add('playerImage');
+            userImage.appendChild(img1);
+
+            const img2 = document.createElement('img')
+            img2.src = 'images/rock.png';
+            img2.classList.add('compImage');
+            compImage.appendChild(img2);
         }
     }  
 }
 
 function playGame(){
+    
     if (playerScore <= 4 && computerScore <= 4){
         round(userChoice);
 
@@ -93,3 +164,4 @@ function playGame(){
         return;
     }
 };
+
